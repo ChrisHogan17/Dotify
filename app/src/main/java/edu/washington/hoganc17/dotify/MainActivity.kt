88c2223d609d4a0eeb23f4bf14db3f9d1132f744
Chris Hogan <chrishogan17@outlook.com>
2020-04-08 01:23:38 -0700

    Create display of song information and album art

---
+++ app/src/main/java/edu/washington/hoganc17/dotify/MainActivity.kt
@@ -0,0 +1,20 @@
+package edu.washington.hoganc17.dotify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
