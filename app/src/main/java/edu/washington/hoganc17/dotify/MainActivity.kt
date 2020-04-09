39701b35915e8bb0a9ffc4096a28ee64cacceb46
Chris Hogan <chrishogan17@outlook.com>
2020-04-08 18:00:48 -0700

    Add ability to change username

--- app/src/main/java/edu/washington/hoganc17/dotify/MainActivity.kt
+++ app/src/main/java/edu/washington/hoganc17/dotify/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
@@ -14,10 +15,19 @@ import kotlin.random.Random
 class MainActivity : AppCompatActivity() {
 
     private var playCount = Random.nextInt(10, 100000)
+    private lateinit var tvUser: TextView
+    private lateinit var etUser: EditText
+    private lateinit var btnChangeUser: Button
+    private lateinit var btnApply: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvUser = findViewById(R.id.tvUser)
+        etUser = findViewById(R.id.etUser)
+        btnChangeUser = findViewById(R.id.btnChangeUser)
+        btnApply = findViewById(R.id.btnApply)
 
         val tvPlayCount = findViewById<TextView>(R.id.tvPlayCount)
         val playsString = "$playCount plays"
@@ -25,8 +35,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun changeUser(view: View) {
-        val tvUser = findViewById<TextView>(R.id.tvUser)
+        tvUser.visibility = View.INVISIBLE
+        btnChangeUser.visibility = View.INVISIBLE
+        etUser.visibility = View.VISIBLE
+        btnApply.visibility = View.VISIBLE
+    }
 
+    fun applyChanges(view: View) {
+        val newName = etUser.text.toString()
+        tvUser.text = newName
+        tvUser.visibility = View.VISIBLE
+        btnChangeUser.visibility = View.VISIBLE
+        etUser.visibility = View.INVISIBLE
+        btnApply.visibility = View.INVISIBLE
     }
 
     fun playTrack(view: View) {
