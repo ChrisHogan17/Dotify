09b59b8dbc1557f8b15ffcd8fd23e8be0eed7283
Chris Hogan <chrishogan17@outlook.com>
2020-04-20 23:36:36 -0700

    Create a SongListActivity that displays a list of song titles, artists, and album art from the SongDataProvider in a RecyclerView

---
+++ app/src/main/java/edu/washington/hoganc17/dotify/SongListActivity.kt
@@ -0,0 +1,21 @@
+package edu.washington.hoganc17.dotify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ericchee.songdataprovider.SongDataProvider
+import kotlinx.android.synthetic.main.activity_song_list.*
+
+class SongListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_song_list)
+        title = "All Songs"
+
+        val songList = SongDataProvider.getAllSongs()
+
+        val songAdapter = SongAdapter(songList)
+        rvSongs.adapter = songAdapter
+
+    }
+}
